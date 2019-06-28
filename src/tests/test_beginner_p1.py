23df75b659cf43b6006948a70fee784904c6fe92
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-06-28 15:06:59 +0300

    new tests

--- src/tests/test_beginner_p1.py
+++ src/tests/test_beginner_p1.py
@@ -15,3 +15,26 @@ def test_more_lists():
 
     a, b = flatten([], numbers_list, 1)
     assert b == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_no_depth():
+    fruit_list = ['apple', ['cherry', 'banana', ['kiwi', 'mango'], 'strawberry', 'orange'], 'lemon']
+
+    a, b = flatten([], fruit_list, 0)
+    assert b == ['apple', ['cherry', 'banana', ['kiwi', 'mango'], 'strawberry', 'orange'], 'lemon']
+
+
+def test_epmty_list():
+    a, b = flatten([], [], 2)
+    assert a == []
+    assert b == []
+
+
+def test_negative_depth():
+    numbers_list = [1, 2, [3, 4, 5, 6, 7], 8]
+
+    a, b = flatten([], numbers_list, -1)
+    assert a == []
+    assert b == [1, 2, [3, 4, 5, 6, 7], 8]
+
+
