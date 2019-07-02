23e9502730c44540bc6c99ca021e3543fa5fda5c
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-02 16:21:32 +0300

    p2 done

--- src/beginner_p2.py
+++ src/beginner_p2.py
@@ -3,7 +3,6 @@ def merge_two_objects(obj1, obj2):
     new_obj = {}
     obj1_keys_list = list(obj1)
     obj2_keys_list = list(obj2)
-    print(obj1_keys_list)
 
     for key in obj1_keys_list :
         if key in obj2_keys_list:
@@ -16,45 +15,27 @@ def merge_two_objects(obj1, obj2):
                 if isinstance(obj1_value, int) or isinstance(obj1_value, (list,)) or isinstance(obj1_value, str) or isinstance(obj1_value, float):  # integer, float, list, string
                     new_val = obj1_value + obj2_value
                     new_obj[key] = new_val
-                    #print(new_obj[key])
                 elif isinstance(obj1_value, set):  # set
                     new_set = obj1_value.union(obj2_value)
                     new_obj[key] = new_set
-                elif isinstance(obj1_value, dict):  # recursive?
+                elif isinstance(obj1_value, dict):  # recursive
                     temp_obj = merge_two_objects(obj1_value, obj2_value)
                     new_obj[key] = temp_obj
 
             obj2_keys_list.remove(key)
         else:
             # this key from obj1 has no match in obj2
-            new_obj = add_without_merging(key, obj1, new_obj)
+            new_obj = add_elem_without_merging(key, obj1, new_obj)
 
     if len(obj2_keys_list) > 0:
         # some keys from obj2 do not have a match in obj1
         # and will be added without merging
         for key in obj2_keys_list:
-            new_obj = add_without_merging(key, obj2, new_obj)
+            new_obj = add_elem_without_merging(key, obj2, new_obj)
 
-    print(new_obj)
     return new_obj
 
 
-
-def add_without_merging(key, old_obj, new_obj):
+def add_elem_without_merging(key, old_obj, new_obj):
     new_obj[key] = old_obj[key]
     return new_obj
-
-
-
-a = {'a': 1.2, 'x': [1,2,3], 'y': 1, 'z': set([1,2,3]), 'w': 'qweqwe', 't': {'a': [1, 2], 'b': 'ddd'}}
-b = {'a': 1.3, 'x': [4,5,6], 'y': 4, 'z': set([4,2,3]), 'w': 'asdf', 't': {'a': [3, 2], 'b': 'eee'}, 'm': "wer"}
-
-
-#a = {'x': [1,2,3], 'y': 1}
-#b = {'x': [4,5,6], 'y': 4}
-
-
-merge_two_objects(a, b)
-
-
-
--- src/tests/test_beginner_p2.py
+++ src/tests/test_beginner_p2.py
@@ -5,19 +5,33 @@ def test_simple():
     b = {'x': [4,5,6], 'y': 4, 'z': set([4,2,3]), 'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer"}
 
     new_obj = merge_two_objects(a, b)
-    assert new_obj == {'x': [1,2,3,4,5,6], 'y': 5, 'z': set([1,2,3,4]), 'w': 'qweqweasdf', 't': {'a': [1, 2, 3,
-                        2]}, 'm': ([1], "wer")}
+    assert new_obj == {'x': [1,2,3,4,5,6], 'y': 5, 'z': set([1,2,3,4]), 'w': 'qweqweasdf', 't': {'a': [1, 2, 3, 2]}, 'm': ([1], "wer")}
+
+
+def test_different_length():
+    a = {'x': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3])}
+    b = {'x': [4, 5, 6], 'y': 4, 'z': set([4, 2, 3]), 'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer"}
+
+    new_obj = merge_two_objects(a, b)
+    assert new_obj == {'x': [1, 2, 3, 4, 5, 6], 'y': 5, 'z': set([1, 2, 3, 4]), 'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer"}
+
+
+def test_keys_not_matching():
+    a = {'a': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3]), 'b': 'qweqwe', 't': {'a': [1, 2]}, 'm': [1]}
+    b = {'x': [4, 5, 6], 'y': 4, 'z': set([4, 2, 3]), 'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer"}
+
+    new_obj = merge_two_objects(a, b)
+    assert new_obj == {'a': [1, 2, 3], 'y': 5, 'z': set([1, 2, 3, 4]), 'b': 'qweqwe', 't': {'a': [1, 2, 3, 2]}, 'm': ([1], "wer"), 'x': [4, 5, 6], 'w': 'asdf'}
+
+
+def test_keys_matching_but_different_order():
+    a = {'x': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3]), 'w': 'qweqwe', 't': {'a': [1, 2]}, 'm': [1]}
+    b = {'y': 4, 'w': 'asdf', 't': {'a': [3, 2]}, 'm': "wer", 'x': [4, 5, 6], 'z': set([4, 2, 3])}
+
+    new_obj = merge_two_objects(a, b)
+    assert new_obj == {'x': [1,2,3,4,5,6], 'y': 5, 'z': set([1,2,3,4]), 'w': 'qweqweasdf', 't': {'a': [1, 2, 3, 2]}, 'm': ([1], "wer")}
 
 
 
-
-
-
-
-
-# intrebari: 1. Obiectele au mereu aceeasi lungime(numar de elemente)?
-#            2. Cheile obiectelor se potrivesc mereu?
-#            3. key-urile sunt in aceeasi ordine?
-#            3. sau trebuie test case-uri pt cazurile de mai sus?
 
 
