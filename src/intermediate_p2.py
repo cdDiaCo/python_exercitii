166a6e51a9e9a78e3858668a9a89c2a3a2dbccfe
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-17 15:57:34 +0300

    now class Desk extends UserList

--- src/intermediate_p2.py
+++ src/intermediate_p2.py
@@ -1,53 +1,68 @@
+
+
+from collections import UserList
 import random
 
 
-class Deck :
-    def __init__(self):
-        self.cards = []
 
+
+class Deck(UserList) :
+
+    def __init__(self):
+        UserList.__init__(self)
         colors = ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"]
         for color in colors :
-            for elem in range(2,16) :
-                if elem == 11 :
+            for num in range(2,16) :
+                if num == 11 :
                     continue
-                new_card = (elem, color)
+                new_card = (num, color)
                 self.add_card(new_card)
 
 
+
     def shuffle_cards(self):
-        middle = int(len(self.cards)/2)
-        num_of_cards = len(self.cards)
+        middle = int(len(self)/2)
+        num_of_cards = len(self)
 
         for i in range(1, middle):
             first_random_card_position = random.randint(1, middle)
             second_random_card_position = random.randint(middle+1, num_of_cards-1)
-            temp = self.cards[first_random_card_position]
-            self.cards[first_random_card_position] = self.cards[second_random_card_position]
-            self.cards[second_random_card_position] = temp
+            temp = self[first_random_card_position]
+            self[first_random_card_position] = self[second_random_card_position]
+            self[second_random_card_position] = temp
+
 
 
     def sort_cards_by_number(self):
         def sortSecond(val):
             return val[0]
 
-        new_deck.cards.sort(key=sortSecond)
+        new_deck.sort(key=sortSecond)
+
 
 
     def sort_cards_by_color(self):
         def sortFirst(val):
             return val[1]
 
-        new_deck.cards.sort(key=sortFirst)
+        new_deck.sort(key=sortFirst)
 
 
 
     def add_card(self, card):
         # check if card already exists?
-        self.cards.append(card)
+        if card in self:
+            print('Already in the Deck.')
+        else:
+            self.append(card)
+
 
 
     def remove_card(self, card):
-        self.cards.remove(card)
+        if card in self:
+            self.remove(card)
+        else:
+            print("Card cannot be removed. Card not present in the deck")
 
 
 
@@ -77,30 +92,45 @@ class Hand :
         pass
 
 
+class Player :
+    def __init__(self):
+        pass
+
+
+class Table :
+    def __init__(self):
+        pass
+
+
+class Dealer :
+    def __init__(self):
+        pass
+
 
 
 
 new_deck = Deck()
 
-#print(len(new_deck.cards))
+
+#print(new_deck)
 #new_deck.shuffle_cards()
-#print(new_deck.cards)
+#print(new_deck)
 
 #new_deck.sort_cards_by_color()
 #new_deck.sort_cards_by_number()
 
 
-#print(new_deck.cards)
 
 card = (3, 'SPADES')
 
-#new_deck.remove_card(card)
+new_deck.remove_card(card)
 
+new_deck.remove_card(card)
 
-print(new_deck.cards)
 
 #new_deck.add_card(card)
 
-#print("-----------------")
-#print(new_deck.cards)
+print(new_deck)
+
+
 
