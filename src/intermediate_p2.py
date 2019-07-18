d1afb596cb50e356e8588c58c01f7b96e2aeea1e
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-18 18:33:42 +0300

    end of day commit

--- src/intermediate_p2.py
+++ src/intermediate_p2.py
@@ -1,5 +1,10 @@
 
 
+# TODO when cards are dealt, remove them from dealerDeck, and add them to the other decks
+# TODO add new methods for some classes to avoid a 'long chain of calls'
+# TODO try to change the sort methods
+
+
 from collections import UserList
 import random
 
@@ -21,6 +26,7 @@ class Deck(UserList) :
 
 
     def remove_card(self, card):
+        print("in remove")
         if card in self:
             self.remove(card)
         else:
@@ -42,10 +48,12 @@ class HandDeck(Deck):
 
 
 class DealerDeck(Deck):
+    colors = ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"]
+
     def __init__(self):
         Deck.__init__(self)
-        colors = ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"]
-        for color in colors:
+
+        for color in self.colors:
             for num in range(2, 16):
                 if num == 11:
                     continue
@@ -65,16 +73,56 @@ class DealerDeck(Deck):
             self[second_random_card_position] = temp
 
 
+
     def sort_cards_by_number(self):
-        def sortSecond(val):
-            return val[0]
-        new_deck.sort(key=sortSecond)
+        num_of_sorted_cards = 0
+        for num in range(2, 16):
+            if num == 11:
+                continue
+
+            for i in range(num_of_sorted_cards, len(self)):
+
+                if self[i].get_card_number() == num :
+                    continue
+                else:
+                    for j in range(i+1, len(self)):
+
+                        if self[j].get_card_number() == num :
+                            temp = self[i]
+                            self[i] = self[j]
+                            self[j] = temp
+
+                            break
+            num_of_sorted_cards += 4
 
 
-    def sort_cards_by_color(self):
+
+    def sort_cards_by_color(self, card):
         def sortFirst(val):
-            return val[1]
-        new_deck.sort(key=sortFirst)
+            return card.get_card_color()
+
+        self.sort(key=sortFirst)
+
+
+'''
+    def sort_cards_by_color(self):
+        num_of_sorted_cards = 0
+        for color in self.colors:
+            for i in range(num_of_sorted_cards, len(self)) :
+
+                if self[i].get_card_color() == color:
+                    continue
+                else:
+                    for j in range(i+1, len(self)):
+                        if self[j].get_card_color() == color:
+                            temp = self[i]
+                            self[i] = self[j]
+                            self[j] = temp
+
+                            break
+            num_of_sorted_cards += 13
+'''
+
 
 
 
@@ -91,11 +139,16 @@ class TableDeck(Deck):
 
 
 
-class Card :
+class Card() :
+
     def __init__(self, card_number, card_color):
         self.card_number = card_number
         self.card_color = card_color
-        pass
+
+    def __eq__(self, other):
+        if self.card_number == other.card_number and self.card_color == other.card_color :
+            return True
+        return False
 
 
     def get_card_color(self):
@@ -106,18 +159,22 @@ class Card :
         return self.card_number
 
 
+    def get_card_description(self):
+        description = self.get_card_color() + " " + str(self.get_card_number())
+        return  description
 
 
 
+'''
 class Hand() :
     def __init__(self):
         self.hand_deck = HandDeck()
-
+'''
 
 
 class Player :
     def __init__(self):
-        self.hand = Hand()
+        self.hand = HandDeck()
 
 
 
@@ -141,32 +198,67 @@ class Dealer :
 
 
 
-
-
-new_deck = Deck()
-
-
-#print(new_deck)
-#new_deck.shuffle_cards()
-#print(new_deck)
-
-#new_deck.sort_cards_by_color()
-#new_deck.sort_cards_by_number()
+def print_cards(deck):
+    for elem in deck:
+        print(elem.get_card_description())
 
 
 
-card = Card(3, 'SPADES')
-
-#new_deck.remove_card(card)
-
-#new_deck.remove_card(card)
+new_dealer = Dealer()
+new_table = Table(new_dealer)
 
 
-#new_deck.add_card(card)
+player_one = Player()
+player_two = Player()
 
-#print(len(new_deck))
+
+new_table.add_player(player_one)
+new_table.add_player(player_two)
 
 
-new_hand = Hand()
 
-print(len(new_hand))
+
+dealer_deck = DealerDeck()
+dealer_deck.shuffle_cards()
+
+
+card_one = Card(15, "SPADES")
+
+
+
+dealer_deck.sort_cards_by_color(card_one)
+#dealer_deck.sort_cards_by_number()
+
+
+
+#card_two = Card(15, "HEARTS")
+#card_three = Card(15, "DIAMONDS")
+#dealer_deck.remove_card(card_one)
+#dealer_deck.remove_card(card_two)
+#dealer_deck.remove_card(card_three)
+
+#dealer_deck.add_card(card_one)
+
+
+
+
+#player_two.hand.add_card(card_one)
+#player_two.hand.add_card(card_two)
+
+
+#new_table.deck.add_card(card_three)
+#new_table.deck.remove_card(card_three)
+
+
+#print(new_table.deck)
+
+#print(new_card.get_card_description())
+
+
+print_cards(dealer_deck)
+
+
+
+
+
+
