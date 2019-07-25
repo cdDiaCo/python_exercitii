511492ade3f22e759f2bec0443332962907dcbcf
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-25 14:59:07 +0300

    yield sets not tuples

--- src/advanced_p2.py
+++ src/advanced_p2.py
@@ -7,9 +7,15 @@ new_set = {1, 2, 3}
 def my_generator(my_set):
     for i in range(len(my_set)+1):
         for comb in itertools.combinations(my_set, i):
-            yield comb
+            output = set()
+            for el in comb:
+                output.add(el)
+            yield output
 
 
 
 for subset in my_generator(new_set):
     print(subset)
+
+
+
