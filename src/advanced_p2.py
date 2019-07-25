b9925bcab753618bd5af2182e4df1ec07f6b095c
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-25 16:23:49 +0300

    small change

--- src/advanced_p2.py
+++ src/advanced_p2.py
@@ -7,10 +7,7 @@ new_set = {1, 2, 3}
 def my_generator(my_set):
     for i in range(len(my_set)+1):
         for comb in itertools.combinations(my_set, i):
-            output = set()
-            for el in comb:
-                output.add(el)
-            yield output
+            yield set(comb)
 
 
 
