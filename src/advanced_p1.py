7e81059e09d25887415a3a208ec3a6736a2cf1dd
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-25 10:59:52 +0300

    add recursivity inside iterator

--- src/advanced_p1.py
+++ src/advanced_p1.py
@@ -1,44 +1,58 @@
 
 
+tt = ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
+
+
+
 # use iterator
 class MyIterator:
     def __init__(self, list):
         self.list = list
         self.index = 0
+        self.my_iter = None
+
 
     def __iter__(self):
         return self
 
+
     def __next__(self):
+        if self.my_iter:
+            try:
+                return next(self.my_iter)
+            except StopIteration:
+                self.my_iter = None
+                return next(self)
         try:
             node = self.list[self.index]
         except IndexError:
             raise StopIteration
         self.index += 1
-        return node
 
-
-
-def print_iterator_output(iterator):
-    while True:
-        try:
-            node = next(iterator)
-        except StopIteration:
-            break
+        if isinstance(node, tuple, ):
+            self.my_iter = MyIterator(node)
+            return next(self.my_iter)
+        elif node is not None:
+            return node
         else:
-            if isinstance(node, tuple,):
-                print_iterator_output(MyIterator(node))
-            else:
-                if node is not None:
-                    print(node)
+            return next(self)
 
 
-tt = ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
+    def __str__(self):
+        return "Interator pentru {}".format(self.list)
+
+
+
+
+def print_iterator_output_using_for(iterator):
+    for node in iterator:
+        print(node)
+
 
 output_iterator = MyIterator(tt)
 
-print_iterator_output(output_iterator)
-
+print_iterator_output_using_for(output_iterator)
+#print([node for node in output_iterator])
 
 
 
@@ -54,17 +68,22 @@ def my_generator(list):
                 yield el
 
 
-tt = ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
+def print_generator_output(output):
+    while True:
+        try:
+            node = next(output)
+        except StopIteration:
+            break
+        else:
+            print(node)
 
-output_generator = my_generator(tt)
 
-
-while True:
-    try:
-        node = next(output_generator)
-    except StopIteration:
-        break
-    else:
+def print_generator_output_using_for(output):
+    for node in output:
         print(node)
 
 
+output_generator = my_generator(tt)
+
+#print_generator_output(output_generator)
+#print_generator_output_using_for(output_generator)
